41ee6bba3e0d5477524b7d27e8811b977dce9b4e
Taylor Savage <taylorsavage93@gmail.com>
2022-07-01 12:23:45 -0400

    added a file for variable practice and consolewritline

---
+++ Variables.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int age = 24;
+String myName = "Taylor Savage";
+bool myAge = true;
+Double birthYear = 1998;
+Decimal Price = 2.50m;
+Char grade = 'c';
+
+Console.WriteLine(myName);
+Console.WriteLine(age);
+Console.WriteLine(myAge);
+Console.WriteLine(birthYear);
+Console.WriteLine(grade);
+Console.WriteLine(Price);
+
+
+
